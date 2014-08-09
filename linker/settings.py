31dc45e7d611e3fdd1a739fd4719f2fc8bce4219
Nivedita Muthusubramanian <nivedita-2608@nivedita-2608.csez.zohocorpin.com>
2014-08-09 18:35:45 +0530

    updating all datasets

--- linker/settings.py
+++ linker/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'NAME': 'links',
         'USER': 'root',
-        'PASSWORD': 'nive',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
